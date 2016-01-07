49eaca6df1186c59070446c1a5a7af6d87d78d02
Artem Denysov <denysov.artem@gmail.com>
2016-01-07 13:49:40 +0200

    update coverage

--- karma.conf.js
+++ karma.conf.js
@@ -2,12 +2,20 @@ module.exports = function(config) {
     config.set({
         browsers: ['PhantomJS'],
         frameworks: ['mocha', 'chai', 'es6-shim'],
-        reporters: ['progress'],
         files: [
             "public/javascripts/vendor.js",
             "public/javascripts/app.js",
             "public/javascripts/specs.js"
         ],
+        reporters: ['coverage', 'coveralls'],
+        preprocessors: {
+            'public/javascripts/app.js': 'coverage'
+        },
+        coverageReporter: {
+            type: 'lcov',
+            dir: 'coverage/',
+            subdir: '.'
+        },
         singleRun: true
     });
 };
