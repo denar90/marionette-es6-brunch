3a8754f914170dbf81ed3079402cb8d8f5f4d86d
Artem Denysov <denysov.artem@gmail.com>
2016-04-07 15:23:30 +0300

    improve specs

--- specs/initialize.js
+++ specs/initialize.js
@@ -1,4 +1,5 @@
 import app from './application';
 import tableModel from './tableModel';
 import tableCollection from './tableCollection';
+import itemView from './itemView';
 import tableView from './tableView';
